2730a379b0603e19b62623e9a270d959d54899a9
Jannis R <mail@jannisr.de>
2018-04-21 12:53:09 +0200

    read travelTimes und trips

--- index.js
+++ index.js
@@ -110,6 +110,79 @@ const readRouteStops = (routes, readFile, done) => {
 	)
 }
 
+const readTravelTimes = (readFile, done) => {
+	const time = Symbol('time')
+	const writeTravelTimes = (travelTimes, row, cb) => {
+		const routeId = row.LINE_NR.trim() + '-' + row.STR_LINE_VAR.trim()
+		const id = routeId + '-' + row.TIMING_GROUP_NR.trim()
+		let travelTime = travelTimes[id]
+		if (!travelTime) {
+			travelTime = travelTimes[id] = {
+				type: 'schedule',
+				id,
+				route: routeId,
+				sequence: [],
+				starts: [],
+				[time]: 0
+			}
+		}
+		const arrival = travelTime[time] + parseInt(row.TT_REL)
+		const departure = arrival + parseInt(row.STOPPING_TIME)
+		travelTime[time] = departure
+		travelTime.sequence.push({arrival, departure})
+		cb()
+	}
+
+	reduce(
+		Object.create(null),
+		writeTravelTimes,
+		readFile('lid_travel_time_type.din'),
+		done
+	)
+}
+
+const readTrips = (restrictions, travelTimes, readFile, done) => {
+	const writeTrips = (travelTimes, row, cb) => {
+		const travelTimeId = [
+			row.LINE_NR.trim(),
+			row.STR_LINE_VAR.trim(),
+			row.TIMING_GROUP_NR.trim()
+		].join('-')
+		const travelTime = travelTimes[travelTimeId]
+		if (!travelTime) {
+			console.error('unknown travelTime id ' + travelTimeId)
+			return cb() // skip row
+		}
+
+		const restrictionId = row.RESTRICTION.trim()
+		if (!restrictionId) {
+			// todo: use row.DAY_ATTRIBUTE_NR here
+			if (row.DAY_ATTRIBUTE_NR.trim() === '0') {
+				return cb() // silently skip row
+			}
+			return cb(new Error(`invalid restriction id "${restrictionId}"`))
+		}
+		const restriction = restrictions[restrictionId]
+		if (!restriction) {
+			return cb(new Error('unknown restriction id ' + restrictionId))
+		}
+
+		const dayOffset = parseInt(row.DEPARTURE_TIME)
+		for (let day of restriction.days) {
+			travelTime.starts.push(day + dayOffset)
+		}
+		// todo: VEH_TYPE_NR, DAY_ATTRIBUTE_NR, ROUND_TRIP_ID, TRAIN_NR
+		cb()
+	}
+
+	reduce(
+		travelTimes,
+		writeTrips,
+		readFile('rec_trip.din'),
+		done
+	)
+}
+
 const createReader = (readFile, done) => {
 	readRestrictions(readFile, (err, restrictions) => {
 		if (err) return done(err)
